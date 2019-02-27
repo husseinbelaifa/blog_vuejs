0764f14ab01904fda983caeb3f678780ab648726
belaifa_el_hussein <belaifahocine@gmail.com>
2019-02-27 10:16:33 +0100

    posts

--- src/router.js
+++ src/router.js
@@ -7,8 +7,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
@@ -19,7 +18,12 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/Post',
+      name: 'Post',
+      component: () => import( /* webpackChunkName: "about" */ './views/Post.vue')
+    },
   ]
 })
---
+++ src/views/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <main class="posts-listing col-lg-8">
+        <Post/>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import "@/assets/vendor/bootstrap/css/bootstrap.min.css";
+import "@/assets/vendor/font-awesome/css/font-awesome.min.css";
+import "@/assets/css/fontastic.css";
+import "@/assets/https://fonts.googleapis.com/css?family=Open+Sans:300,400,700";
+import "@/assets/vendor/@fancyapps/fancybox/jquery.fancybox.min.css";
+import "@/assets/css/style.default.cs";
+import "@/assets/vendor/css/custom.css";
+
+import Post from "@/components/Blog/Post.vue";
+
+export default {
+  name: "Post",
+  components: {
+    Post
+  }
+};
+</script>
