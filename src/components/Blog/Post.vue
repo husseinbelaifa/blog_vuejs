438a96f7342a8ca452bb4204ffd9fec6861e37d9
belaifa_el_hussein <belaifahocine@gmail.com>
2019-02-27 09:59:35 +0100

    post

--- src/components/Blog/Post.vue
+++ src/components/Blog/Post.vue
@@ -1,13 +1,44 @@
 <template>
   <div class="container">
     <div class="row">
-      <ul class="post col-xl-6" v-for="post in posts">
+      <div class="col-xl-6" v-for="post in posts " :key="post.id">
         <div class="post-thumbnail">
           <a href="post.html">
-            <img src="img/blog-post-1.jpeg" alt="..." class="img-fluid">
+            <img src="post.thumb" alt="..." class="img-fluid">
           </a>
         </div>
-      </ul>
+        <div class="post-details">
+          <div class="post-meta d-flex justify-content-between">
+            <div class="date meta-last">{{ post.published_at }}</div>
+            <div class="category">
+              <a href="#">{{ post.categories }}</a>
+            </div>
+          </div>
+          <a href="post.html">
+            <h3 class="h4">{{ post.name }}</h3>
+          </a>
+          <p
+            class="text-muted"
+          >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+          <footer class="post-footer d-flex align-items-center">
+            <a href="#" class="author d-flex align-items-center flex-wrap">
+              <div class="avatar">
+                <img src=" post.author.avatar" alt="..." class="img-fluid">
+              </div>
+              <div class="title">
+                <span>{{ post.author.name }}</span>
+              </div>
+            </a>
+            <div class="date">
+              <i class="icon-clock"></i> 2 months ago
+            </div>
+            <div class="comments meta-last">
+              <i class="icon-comment"></i>
+              {{ post.comments_count }}
+            </div>
+          </footer>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,12 +56,12 @@ export default {
   name: "Post",
   data() {
     return {
-      posts: null
+      posts: []
     };
   },
   mounted() {
     axios.get("https://fakeblog.bel4.com/api/posts").then(function(response) {
-      this.posts = response.data;
+      this.posts.push(response.data.data);
     });
   }
 };
